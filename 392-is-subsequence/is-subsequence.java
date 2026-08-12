2cca522196e633632e6cdf3b85b1b7af55fec4bc
Manasa13-k <korrapatimanasalakshmi@gmail.com>
2026-08-12 22:47:29 +0530

    Time: 2 ms (73.73%) | Memory: 42.9 MB (26.16%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -11,10 +11,6 @@ class Solution {
                 j++;
             }
         }
-        if(i==s.length()){
-            return true;
-        }else{
-            return false;
-        }
+        return i== s.length();
     }
 }
